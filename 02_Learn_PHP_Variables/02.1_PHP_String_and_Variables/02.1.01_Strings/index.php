8f44bad103e148436dff9d77ff79c0590416aa90
Sadia Iqbal <138330472+Sadiaiqbal23@users.noreply.github.com>
2023-07-11 20:14:38 +0600

    Update index.php

--- 02_Learn_PHP_Variables/02.1_PHP_String_and_Variables/02.1.01_Strings/index.php
+++ 02_Learn_PHP_Variables/02.1_PHP_String_and_Variables/02.1.01_Strings/index.php
@@ -1,5 +1,11 @@
 <?php
-  //A string is a sequence of characters, like "Hello world!".
-  // https://youtu.be/ek15sBTquKo
-  // Write your code below:
+/* A string is a sequence of characters, like "Hello world!".
+  A string literal can be specified in four different ways: 
+    single quoted
+    double quoted
+    heredoc syntax
+    nowdoc syntax 
+ // https://youtu.be/ek15sBTquKo
+ // Write your code below:
+*/
   echo "Hello, World!";
